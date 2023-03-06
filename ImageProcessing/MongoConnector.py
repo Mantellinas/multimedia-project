225a53df9b76735288f53984fcf9e9b8319e22d8
Rosario Cannavò <rosariocannavo2000@gmail.com>
2023-03-06 13:35:06 +0100

    feat: "added mongodb bson correct retrieval"

--- ImageProcessing/MongoConnector.py
+++ ImageProcessing/MongoConnector.py
@@ -1,23 +1,12 @@
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 import os
-import json
-import bson
+from bson.json_util import dumps
 
 
-# MONGO_USR = os.getenv('MONGO_USER')
-# MONGO_PSW = os.getenv('MONGO_PASSWORD')
-# MONGO_DB = os.getenv('MONGO_DATABASE')
-MONGO_USR = "myuser"
-MONGO_PSW = "mypassword"
-MONGO_DB = "image_database"
-
-class JSONEncoder(json.JSONEncoder):
-    def default(self, o):
-        if isinstance(o, ObjectId):
-            return str(o)
-        return json.JSONEncoder.default(self, o)
-
+MONGO_USR = os.getenv('MONGO_USER')
+MONGO_PSW = os.getenv('MONGO_PASSWORD')
+MONGO_DB = os.getenv('MONGO_DATABASE')
 
 class MongoConnector:
     def __init__(self):
@@ -28,10 +17,7 @@ class MongoConnector:
         self.mongo_db   = MONGO_DB
         self.connection = None
         
-
-    # CONNECTION OPENING
     def connection_opening(self):
-        #print("DATABASE CONNECTION OPENING")
         try:
             self.connection = MongoClient(
                 host        = self.mongo_ip,
@@ -45,7 +31,6 @@ class MongoConnector:
             print(e)
             self.connection = None
 
-    # CONNECTION CLOSING
     def connection_closing(self):
         print("DATABASE CONNECTION CLOSING")
         if self.connection is None: return
@@ -60,7 +45,6 @@ class MongoConnector:
         except Exception as e:
             return None, None
 
-    # DATABASE AND COLLECTION SELECTION
     def getDocumentById(self, collection_name , _id):
         database, collection = self.database_collection_selection(collection_name)
         doc = collection.find_one(ObjectId(_id))
@@ -69,14 +53,12 @@ class MongoConnector:
     def writeDocument(self, collection_name, document):
         database, collection = self.database_collection_selection(collection_name)
         collection.insert_one(document)
-        #print(database.list_collection_names())
 
     def getAllDocument(self, collection_name):
         database, collection = self.database_collection_selection(collection_name)
-        ob = []
-        cursor = collection.find({})
-        for doc in cursor:
-             ob.append(doc)
+        cursor = collection.find()
+        list_cur = list(cursor)
+        return dumps(list_cur, indent = 2)
         
 
         
--- ImageProcessing/processingAPI.py
+++ ImageProcessing/processingAPI.py
@@ -1,16 +1,13 @@
 from flask import Flask
 from MongoConnector import *
 import os
+from bson import *
 
-# KMEANSCOLLECTION = os.getenv('CLUSTERING_KMEANS')
-# HOGSCOLLECTION = os.getenv('CLUSTERING_HOG')
-# FASTCOLLECTION = os.getenv('FAST')
-# SLICCOLLECTION = os.getenv('SLIC')
-# BASECOLLECTION = os.getenv('BASE_IMAGE')
-
-
-
-
+KMEANSCOLLECTION = os.getenv('CLUSTERING_KMEANS')
+HOGSCOLLECTION = os.getenv('CLUSTERING_HOG')
+FASTCOLLECTION = os.getenv('FAST')
+SLICCOLLECTION = os.getenv('SLIC')
+BASECOLLECTION = os.getenv('BASE_IMAGE')
 
 ProcessingAPI = Flask(__name__)
 
@@ -18,31 +15,31 @@ mongo_connector = MongoConnector()
 
 @ProcessingAPI.route("/getAllImages")
 def getAllImages():
-    images = mongo_connector.getAllDocument("base_image")
+    images = mongo_connector.getAllDocument(BASECOLLECTION)
     return images
-
-# @ProcessingAPI.route("/runKmeans")
-# def runKmeans():
-#     images = mongo_connector.ge(KMEANSCOLLECTION)
-#     return images
+   
+@ProcessingAPI.route("/runKmeans")
+def runKmeans():
+    images = mongo_connector.ge(KMEANSCOLLECTION)
+    return images
     
    
-# @ProcessingAPI.route("/runHog")
-# def runHog():
-#     images = mongo_connector.getAllDocument(HOGSCOLLECTION)
-#     return images
+@ProcessingAPI.route("/runHog")
+def runHog():
+    images = mongo_connector.getAllDocument(HOGSCOLLECTION)
+    return images
        
 
-# @ProcessingAPI.route("/runFAST")
-# def runFAST():
-#     images = mongo_connector.getAllDocument(FASTCOLLECTION)
-#     return images
+@ProcessingAPI.route("/runFAST")
+def runFAST():
+    images = mongo_connector.getAllDocument(FASTCOLLECTION)
+    return images
 
 
-# @ProcessingAPI.route("/runSLIC")
-# def runSLIC():
-#     images = mongo_connector.getAllDocument(SLICCOLLECTION)
-#     return images
+@ProcessingAPI.route("/runSLIC")
+def runSLIC():
+    images = mongo_connector.getAllDocument(SLICCOLLECTION)
+    return images
     
 if __name__ == "__main__":
     ProcessingAPI.run(
