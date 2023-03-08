00058754c72daaa78828258e92aedbbc8341866b
Rosario Cannavò <rosariocannavo2000@gmail.com>
2023-03-08 14:18:53 +0100

    feat: "added KMeans class and related cronJob"

---
+++ ImageProcessing/Clustering_KMeans_PCA.py
@@ -0,0 +1,68 @@
+from keras.applications.vgg16 import VGG16 
+from keras.models import Model
+from keras.applications.vgg16 import preprocess_input 
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from PIL import Image
+from MongoConnector import *
+import numpy as np
+import base64
+import json
+import io
+
+COLLECTION = os.getenv('BASE_IMAGE')
+OUTPUTCOLLECTION = os.getenv('CLUSTERING_KMEANS')
+
+class Clustering_KMeans_PCA:
+    def __init__(self):
+        self.mongo_connector = MongoConnector()
+        
+    def extract_features(self,file, model):
+        image = Image.open(io.BytesIO(file))
+        image = image.resize((224, 224))
+        img = np.array(image) 
+        reshaped_img = img.reshape(1,224,224,3) 
+        imgx = preprocess_input(reshaped_img)
+        features = model.predict(imgx, use_multiprocessing=True)
+        return features
+
+    def computate(self):
+        Names = []  
+        model = VGG16()
+        model = Model(inputs = model.inputs, outputs = model.layers[-2].output)
+
+        results = self.mongo_connector.getAllDocument(COLLECTION)
+        results = json.loads(results)
+
+        res = []
+        for entry in results:
+            res.append(base64.b64decode(entry['img']['$binary']['base64']))
+            Names.append(entry['_id'])
+            
+        data = {}
+        i=0
+        for img in res:
+            if i>10:
+                break
+            i+=1
+            feat = self.extract_features(img,model)
+            data[img] = feat
+
+        filenames = np.array(list(data.keys()))
+        feat = np.array(list(data.values()))
+        feat = feat.reshape(-1,4096)
+        
+        n_comp = feat.shape[0]
+
+        pca = PCA(n_components=n_comp, random_state=22)
+        pca.fit(feat)
+        x = pca.transform(feat)
+
+        kmeans = KMeans(n_clusters=3)
+        kmeans.fit(x)
+
+        for i in range(n_comp):
+            cluster = {'base_image_id':Names[i]['$oid'], 'cluster': int(kmeans.labels_[i])}
+            print(cluster)
+            self.mongo_connector.writeDocument(OUTPUTCOLLECTION,cluster)
+
--- ImageProcessing/processingAPI.py
+++ ImageProcessing/processingAPI.py
@@ -1,56 +1,15 @@
-from flask import Flask
-from MongoConnector import *
-import os
 from apscheduler.schedulers.background import BackgroundScheduler
-from bson import *
-
-KMEANSCOLLECTION = os.getenv('CLUSTERING_KMEANS')
-HOGSCOLLECTION = os.getenv('CLUSTERING_HOG')
-FASTCOLLECTION = os.getenv('FAST')
-SLICCOLLECTION = os.getenv('SLIC')
-BASECOLLECTION = os.getenv('BASE_IMAGE')
-
-def sensor():
-    """ Function for test purposes. """
-    print("Scheduler is alive!")
-
-sched = BackgroundScheduler(daemon=True)
-sched.add_job(sensor,'interval',minutes=1) #hours
-sched.start()
+from Clustering_KMeans_PCA import * 
+from flask import Flask
 
 ProcessingAPI = Flask(__name__)
 
-mongo_connector = MongoConnector()
+x = Clustering_KMeans_PCA()
+sched = BackgroundScheduler(daemon=True)
+sched.add_job(x.computate,'interval',minutes=1) #hours
+sched.start()
 
-@ProcessingAPI.route("/getAllImages")
-def getAllImages():
-    images = mongo_connector.getAllDocument(BASECOLLECTION)
-    return images
-   
-@ProcessingAPI.route("/runKmeans")
-def runKmeans():
-    images = mongo_connector.getAllDocument(KMEANSCOLLECTION)
-    return images
-    
-   
-@ProcessingAPI.route("/runHog")
-def runHog():
-    images = mongo_connector.getAllDocument(HOGSCOLLECTION)
-    return images
-       
-
-@ProcessingAPI.route("/runFAST")
-def runFAST():
-    images = mongo_connector.getAllDocument(FASTCOLLECTION)
-    return images
-
-
-@ProcessingAPI.route("/runSLIC")
-def runSLIC():
-    images = mongo_connector.getAllDocument(SLICCOLLECTION)
-    return images
-    
 if __name__ == "__main__":
     ProcessingAPI.run(
             host='0.0.0.0',
-            port=9000)
+            port=8100)
